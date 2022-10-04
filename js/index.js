40d1634606b328b294e0cd9cb1848184dbb625c5
renan-peixoto <renan2.peixoto@gmail.com>
2022-10-04 15:40:32 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+import Controls from "./controls.js";
+import Timer from "./timer.js";
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import { 
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+  secondsDisplay,
+  minutesDisplay 
+} from "./elements.js"
+
+const controls = Controls({
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+})
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset,
+})
+
+
+const sound = Sound()
+
+Events({controls, timer, sound})
+
+
+
